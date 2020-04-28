9ee6607dff1e5e7e41c5dee9ea59799abe9e6ba6
kenaser12345 <kenaser12345@gmail.com>
2020-04-28 15:14:28 +0800

    task-1

---
+++ index.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector(".burger")
+burger.addEventListener('click', function(){
+   const navItem = document.querySelector(".navbar-menu")
+   navItem.classList.add('is-active')
+})
+
